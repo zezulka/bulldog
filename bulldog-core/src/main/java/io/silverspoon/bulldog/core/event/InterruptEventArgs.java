61237b2dd5199709182d301b4c29d7179da77c20
Miloslav Zezulka <zezulka95@gmail.com>
2017-08-10 12:02:56 +0200

    interrupt event args now also returns pin, class refactored to be immutable

--- bulldog-core/src/main/java/io/silverspoon/bulldog/core/event/InterruptEventArgs.java
+++ bulldog-core/src/main/java/io/silverspoon/bulldog/core/event/InterruptEventArgs.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/** *****************************************************************************
  * Copyright (c) 2016 Silverspoon.io (silverspoon@silverware.io)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,22 +12,59 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************/
+ ****************************************************************************** */
 package io.silverspoon.bulldog.core.event;
 
 import io.silverspoon.bulldog.core.Edge;
 import io.silverspoon.bulldog.core.pin.Pin;
 
-public class InterruptEventArgs {
+public final class InterruptEventArgs {
 
-   private Edge edge;
+    private final Edge edge;
+    private final Pin pin;
 
-   public InterruptEventArgs(Pin pin, Edge edge) {
-      this.edge = edge;
-   }
+    public InterruptEventArgs(Pin pin, Edge edge) {
+        this.edge = edge;
+        this.pin = pin;
+    }
 
-   public Edge getEdge() {
-      return edge;
-   }
+    public Edge getEdge() {
+        return edge;
+    }
+
+    public Pin getPin() {
+        return pin;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + this.edge.ordinal();
+        hash = 13 * hash + this.pin.getAddress();
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final InterruptEventArgs other = (InterruptEventArgs) obj;
+        return this.edge.equals(other.edge) && this.pin.equals(other.pin);
+    }
+
+    @Override
+    public String toString() {
+        return "InterruptEventArgs{" + "edge=" + edge + ", pin=" + pin.getName() + '}';
+    }
+
+
+
 
 }
